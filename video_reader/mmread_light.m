8e58eb7af35064164d1ae961cbe2d8733b20dcda
The Beast <a.valera@ucl.ac.uk>
2019-10-01 11:37:29 +0100

    read video in chunks

--- video_reader/mmread_light.m
+++ video_reader/mmread_light.m
@@ -1,33 +1,39 @@
-function video = mmread(filename)
+function video_full = mmread_light(filename)
 
     trySeeking = true;
     matlabCommand = '';
-    disableAudio = nargout < 2;
-    disableVideo = false;
-    frames = [];
-
-    fmt = '';
-    FFGrab('build',filename,fmt,double(disableVideo),double(disableAudio),double(trySeeking));
-    FFGrab('setFrames',frames);
-    FFGrab('setMatlabCommand',matlabCommand);
-    FFGrab('doCapture');
-
-    [nrVideoStreams, nrAudioStreams] = FFGrab('getCaptureInfo');
+    video_full = [];
     
-   
-    % loop through getting all of the video data from each stream
-    for i=1:nrVideoStreams
-        [width, height, rate, nrFramesCaptured, nrFramesTotal, totalDuration] = FFGrab('getVideoInfo',i-1);
-        if (nrFramesTotal > 0 && any(frames > nrFramesTotal))
-            warning('mmread:general',['Frame(s) ' num2str(frames(frames>nrFramesTotal)) ' exceed the number of frames in the movie.']);
-        end
+    vidObj = VideoReader(filename);
+    numFrames = ceil(vidObj.FrameRate*vidObj.Duration);
+    batches = [1:1000:numFrames,numFrames+1]; %% FFGrab('doCapture') uses a lot of memory so we do batches of 1000 points
 
-        video = zeros(height * width * 3, nrFramesCaptured, 'uint8');
-        for f = 1:nrFramesCaptured
-            [video(:,f), ~] = FFGrab('getVideoFrame',i-1,f-1);
-            % the data ordering is wrong for matlab images, so permute it
+    for idx = 1:numel(batches(1:end-1))
+        frames = batches(idx):(batches(idx+1)-1);
+        fmt = '';
+        FFGrab('build',filename,fmt,double(false),double(true),double(trySeeking));
+        FFGrab('setFrames',frames);
+        FFGrab('setMatlabCommand',matlabCommand);
+        FFGrab('doCapture');
+
+        [nrVideoStreams, nrAudioStreams] = FFGrab('getCaptureInfo');
+
+
+        % loop through getting all of the video data from each stream
+        for i=1:nrVideoStreams
+            [width, height, rate, nrFramesCaptured, nrFramesTotal, totalDuration] = FFGrab('getVideoInfo',i-1);
+            if (nrFramesTotal > 0 && any(frames > nrFramesTotal))
+                warning('mmread:general',['Frame(s) ' num2str(frames(frames>nrFramesTotal)) ' exceed the number of frames in the movie.']);
+            end
+
+            video = zeros(height * width * 3, nrFramesCaptured, 'uint8');
+            for f = 1:nrFramesCaptured
+                [video(:,f), ~] = FFGrab('getVideoFrame',i-1,f-1);
+                % the data ordering is wrong for matlab images, so permute it
+            end
+            video = permute(reshape(video(1:3:end,:), width, height, nrFramesCaptured),[2 1 3]); % WARNING -- Disabled channel 2 and 3 here
         end
-        video = permute(reshape(video(1:3:end,:), width, height, nrFramesCaptured),[2 1 3]); % WARNING -- Disabled channel 2 and 3 here
+        FFGrab('cleanUp'); 
+        video_full = cat(3, video_full, video);
     end
-    FFGrab('cleanUp');    
 end
