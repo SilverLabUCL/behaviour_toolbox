ad29e5c75bc58d976d01df36cafc28b1974b8200
The Beast <a.valera@ucl.ac.uk>
2019-10-03 15:23:20 +0100

    added inter batch MI stitching

--- Motion_Index/get_MI_from_video.m
+++ Motion_Index/get_MI_from_video.m
@@ -100,8 +100,10 @@ function [motion_indexes, video] = get_MI_from_video(file_path_or_data, timestam
     %% Preparation for ROI collection
     if ~isempty(ROI)
         motion_indexes = cell(1, numel(ROI));
+        interbatch_holder = cell(1, numel(ROI));
     else
         motion_indexes = cell(1, 1);
+        interbatch_holder = cell(1, 1);
     end
     
     %% Set some useful variables
@@ -122,15 +124,16 @@ function [motion_indexes, video] = get_MI_from_video(file_path_or_data, timestam
     
     %% Now collect data, batch by batch
     offset = 0;
-    for idx = 1:n_src  
+    
+    for batch_idx = 1:n_src  
         %% Setup current batch
         if ~dump_data
             %% Whole video at once
-            video = file_path_or_data{idx};
+            video = file_path_or_data{batch_idx};
             clear file_path_or_data;
         else
             %% Current batch
-            video = file_path_or_data.video_full(1, idx);
+            video = file_path_or_data.video_full(1, batch_idx);
             video = video{1};
             nFrames = size(video, 3);
             timestamp = ((1:nFrames) + offset)';
@@ -161,7 +164,14 @@ function [motion_indexes, video] = get_MI_from_video(file_path_or_data, timestam
             %% Preallocate output
             MI = nan(nFrames, 2);  
 
-            %% Compute motion index
+            %% Compute motion index for first point (for batch_idx > 1)
+            if nFrames > 1 && dump_data && batch_idx > 1 
+                X  = interbatch_holder{MI_idx};
+                X1 = local_data(:,:,1);
+                FrameDiff = squeeze(X1(:,:,1)-X(:,:,1));
+                MI(1,1) = (sum(sum(FrameDiff.*FrameDiff)));
+            end
+            %% Compute motion index for the rest
             for i = 2:nFrames
                 X  = local_data(:,:,i-1);
                 X1 = local_data(:,:,i);
@@ -170,6 +180,8 @@ function [motion_indexes, video] = get_MI_from_video(file_path_or_data, timestam
                 % FrameDiff = diff(local_data(:,:,i-1:i),1,3).^2;
                 % MI(i,1) = sum(FrameDiff(:));
             end
+            interbatch_holder{MI_idx} = local_data(:,:,end);
+
             
             %% Normalize
             if normalize
@@ -179,7 +191,7 @@ function [motion_indexes, video] = get_MI_from_video(file_path_or_data, timestam
             end
 
             MI(:,2) = timestamp(1:nFrames,end); %setup time axis
-            %MI = MI(2:end,:); --> need to be recovered for stitching
+            %MI = MI(2:end,:); 
 
             %% Stitch to any previous batch
             motion_indexes{MI_idx} = cat(1, motion_indexes{MI_idx}, MI);
